5240c1e1eaad5447e8f73d83400debb42d5f90c4
ahmed arafa <ahmedarafa.200444@gmail.com>
2023-10-19 19:49:44 +0300

    done

---
+++ free_cmd.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * free_cmd - free_cmd
+ * @cmd: cmd
+ */
+void free_cmd(char **cmd)
+{
+        int i;
+
+        for (i = 0; cmd[i] != NULL; i++)
+        {
+                free(cmd[i]);
+        }
+        free(cmd[i]);
+        free(cmd);
+}
--- functions.c
+++ functions.c
@@ -32,19 +32,3 @@ int combar(char *s1, char *s2)
 	}
 	return (1);
 }
-
-/**
- * free_cmd - free_cmd
- * @cmd: cmd
- */
-void free_cmd(char **cmd)
-{
-	int i;
-
-	for (i = 0; cmd[i] != NULL; i++)
-	{
-		free(cmd[i]);
-	}
-	free(cmd[i]);
-	free(cmd);
-}
